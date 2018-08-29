d9a6cea8843b652d236025d4e4927bd38a8b4940
micimize <rosenthalm93@gmail.com>
2018-08-28 19:29:26 -0500

    slightly better form control props

--- packages/material-ui/src/FormControl/FormControl.d.ts
+++ packages/material-ui/src/FormControl/FormControl.d.ts
@@ -1,15 +1,15 @@
 import * as React from 'react';
+import { ViewProperties, EventEmitterListener as EventHandler } from 'react-native';
 import { StandardProps, PropTypes } from '..';
 
-export interface FormControlProps
-  extends StandardProps<React.HtmlHTMLAttributes<HTMLDivElement>, FormControlClassKey> {
+export interface FormControlProps extends StandardProps<ViewProperties, FormControlClassKey> {
   component?: React.ReactType<FormControlProps>;
   disabled?: boolean;
   error?: boolean;
   fullWidth?: boolean;
   margin?: PropTypes.Margin;
-  onBlur?: React.EventHandler<any>;
-  onFocus?: React.EventHandler<any>;
+  onBlur?: EventHandler<any>;
+  onFocus?: EventHandler<any>;
   required?: boolean;
 }
 
